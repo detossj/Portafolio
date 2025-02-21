316dab2c2baa933bdb2fe958b19481bfca8e5cd4
detossj <jorgerubilarssj@gmail.com>
2025-02-21 17:54:02 -0300

    emailJS added

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,30 +1,59 @@
-import { forwardRef } from "react"
+import { forwardRef, useRef } from "react"
 import '../styles/contactMe/contactMe.css'
 import { FaInstagram, FaWhatsapp } from "react-icons/fa";
+import emailjs from 'emailjs-com'
 
 export const ContactMe = forwardRef((props, ref) => {
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_yg40e3l", // SERVICE ID
+      "template_kqnxqvf", // TEMPLATE ID
+      form.current,
+      "A_-0bPxjCmehlwMrm" // PUBLIC KEY
+    )
+    .then(
+      (result) => {
+        console.log("Correo enviado con éxito", result.text);
+        alert("Mail sent successfully");
+      },
+      (error) => {
+        console.error("Error al enviar el correo", error);
+        alert("There was an error sending the email");
+      }
+    );
+
+    e.target.reset();
+  }
+
   return (
     <section ref={ref}>
       <div className="contact">
+
         <h1 className="title">Contact Me</h1>
-        <form action="" className="about-contact">
+
+        <form className="about-contact" ref={form} onSubmit={sendEmail}>
 
           <label htmlFor="name">Full Name</label>
-          <input type="text" id="name"/>
+          <input type="text" id="name" name="name" required/>
 
           <label htmlFor="email">Email</label>
-          <input type="text" id="email"/>
+          <input type="text" id="email" name="email" required/>
 
           <label htmlFor="accountType">Account Type</label>
-          <select name="accountType" id="">
+          <select id="accountType" name="type" required>
             <option value="">Select an option</option>
-            <option value="option1">Business</option>
-            <option value="option2">Individual</option>
-            <option value="option3">Other</option>
+            <option value="Business">Business</option>
+            <option value="Individual">Individual</option>
+            <option value="Other">Other</option>
           </select>
 
           <label htmlFor="message">Message</label>
-          <textarea id="message"></textarea>
+          <textarea id="message" name="message" required></textarea>
 
           <button className="button" type="submit">Send</button>
 
